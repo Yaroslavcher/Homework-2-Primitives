5f7ec002826b92cd507895e98e0600f82e421730
Ярослав Черников <yaroslavcher@gmail.com>
2022-05-26 00:39:06 +0300

    Corrected Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
         //переменная стоимости билета
-        int s = 5031;
+        int price = 5031;
 
         //переменная количества рублей за одну милю
-        int k = 20;
+        int rubPerMile = 20;
 
         //формула бонусных миль
-        int m = s / k;
+        int bonus = price / rubPerMile;
 
         //вывод на экран количества бонусных миль
-        System.out.println(m + " бонусных миль");
+        System.out.println(bonus + " бонусных миль");
     }
 }
